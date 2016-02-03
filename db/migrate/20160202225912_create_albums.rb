d565c4751be7edb88445c8ee96ba95e7d0b9513e
Harsheek Thanki <harsheekthanki@gmail.com>
2016-02-03 16:07:49 +0000

    Add model and database migration to app

---
+++ db/migrate/20160202225912_create_albums.rb
@@ -0,0 +1,12 @@
+class CreateAlbums < ActiveRecord::Migration
+  def change
+    create_table :albums do |t|
+
+    	t.string :artist
+    	t.string :album
+    	t.text :review
+
+    	t.timestamps
+    end
+  end
+end
